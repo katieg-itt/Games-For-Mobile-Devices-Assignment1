0d83ac67ef0cd11123b7fcd1bae7c9d616ebf041
katieg-itt <katie.griffiths@students.ittralee.ie>
2018-02-23 12:58:14 +0000

    First Commit

---
+++ Assets/CubeControl.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControl : MonoBehaviour {
+    bool Imselected = false;
+
+    public float Scale;
+    private float rotationRate = 3.0F;
+    public float speed = 0.1F;
+
+
+
+
+    Renderer ourRenderer;
+	// Use this for initialization
+	void Start () {
+        ourRenderer = GetComponent<Renderer>();
+        Scale = transform.localScale.magnitude;
+        Input.gyro.enabled = true;
+        Imselected = false;
+       
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        transform.localScale = Scale * Vector3.one;
+        if (Scale <= 0f) Scale = 0.1f;
+
+       transform.rotation = Input.gyro.attitude;
+
+       // transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
+
+
+        //////Accelerometer
+      // Vector3 vect = Vector3.zero;
+
+      //  //remap the device acceleration axis to game coordinates:
+      //  ////// 1) XY plane of the device is mapped onto XZ plane
+      //  ////// 2) rotated 90 degrees around Y axis
+      //  vect.x = -Input.acceleration.y;
+      //  vect.z = Input.acceleration.x;
+
+      //  ////// clamp acceleration vector to the unit cube
+      //if (vect.sqrMagnitude > 1)
+      //     vect.Normalize();
+
+      //  ////// Make it move 10 meters per second instead of 10 meters per frame
+      // vect *= Time.deltaTime;
+
+      //  ////// Move object
+      // transform.Translate(vect * speed);
+
+
+    }
+
+    public void gyroOn(bool on)
+    {
+        if (on == false)
+        {
+            Input.gyro.enabled = false;
+        }
+        else
+        {
+            Input.gyro.enabled = true;
+        }
+
+    }
+    public bool gyroIsOn()
+        {
+            if (Input.gyro.enabled == true)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+      
+
+
+    
+
+    internal void ToggleSelectThisCube()
+    {
+
+        Imselected = !Imselected;
+
+        if (Imselected && (ourRenderer.material.color == Color.white))
+        {
+            ourRenderer.material.color = Color.red;
+        }
+        else if(!Imselected && (ourRenderer.material.color == Color.red))
+        {
+            ourRenderer.material.color = Color.white;
+        }
+    }
+    }
+
---
+++ Assets/CubeTouch.cs
@@ -0,0 +1,346 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeTouch : MonoBehaviour
+{
+
+    enum GestureType { None, Tap, Drag, Scale, Rotate };
+
+    GestureType currentGesture = GestureType.None;
+
+
+
+    public float speed = 0.1F;
+    private Vector3 touchedPos;
+    private float timeTouched;
+    private bool hasMoved;
+    private CubeControl nullSelectedObject = new CubeControl();
+    private CubeControl currentlySelectedObject;
+    private float distanceToSelectedObject;
+    public float perspectiveZoomSpeed = 0.5f;
+    private float startingDistanceBetweenTouches;
+    private float startScale;
+    private bool isItaCube;
+
+
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        currentlySelectedObject = nullSelectedObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeTouched += Time.deltaTime;
+
+        if (Input.touchCount > 0)
+        {
+
+            foreach (Touch touch in Input.touches)
+            {
+
+
+                // Debug.Log("Touching at: " + touch.position);
+                Ray r = Camera.main.ScreenPointToRay(touch.position);
+                // Debug.DrawRay(r.origin, 100 * r.direction);
+
+
+                RaycastHit hit = new RaycastHit();
+                
+               
+
+
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        timeTouched = 0f;
+                        hasMoved = true;
+
+                        
+
+                        if (Physics.Raycast(r, out hit))
+                        {
+                            CubeControl latestCube = hit.collider.gameObject.GetComponent<CubeControl>();
+
+                            if (latestCube)
+                            {
+                                distanceToSelectedObject = Vector3.Distance(Camera.main.transform.position, latestCube.transform.position);
+                            }
+                        }
+
+                       
+
+                        break;
+                    case TouchPhase.Moved:
+
+
+                        switch (currentGesture)
+                        {
+
+                            case GestureType.None:
+                                //Need to determine gesture here
+
+                                //Is it a drag
+                                if ((Input.touchCount == 1) && (timeTouched >= 0.5f))
+                                {
+
+                                    if ((currentlySelectedObject))
+                                    {
+                                        currentGesture = GestureType.Drag;
+
+
+                                    }
+
+                                }
+
+
+                                if (Input.touchCount == 2)
+                                {
+
+                                    Touch touchZero = Input.GetTouch(0);
+                                    Touch touchOne = Input.GetTouch(1);
+
+                                    if (Input.touches[0].fingerId == touch.fingerId)
+                                    {
+                                        startingDistanceBetweenTouches = Vector2.Distance(touchZero.position, touchOne.position);
+
+                                        if ((currentlySelectedObject))  startScale = currentlySelectedObject.Scale;
+                                    }
+                                    else if ((touchOne.phase == TouchPhase.Moved) || (touchZero.phase == TouchPhase.Moved))
+                                    {
+                                        //Calculate distacne difference
+                                        float prevTouchDeltaMag = ((touchZero.position - touchZero.deltaPosition) - (touchOne.position - touchOne.deltaPosition)).magnitude;
+                                        float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+                                        float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+                                        print("magDiff: " + deltaMagnitudeDiff);
+
+                                        //Calculate Scale factor
+                                        float currentDistanceBetweenTouches = Vector2.Distance(touchZero.position, touchOne.position);
+                                        float scaleFactor = currentDistanceBetweenTouches / startingDistanceBetweenTouches;
+                                        print("scaleFactor: " + scaleFactor);
+
+
+                                        // Scale if selected
+                                        if (currentlySelectedObject)
+                                        {
+
+                                            currentlySelectedObject.Scale += -deltaMagnitudeDiff * 0.01f;
+                                        }
+                                        // Zoom if not selected
+                                        else
+                                        {
+
+                                            Camera.main.fieldOfView += deltaMagnitudeDiff * 0.1f;
+                                            Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, 20f, 100f);
+
+
+                                        }
+                                        Vector3 diff = touchOne.position - touchZero.position;
+                                        float angle = Mathf.Atan2(diff.y, diff.x);
+                                        currentlySelectedObject.transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Rad2Deg * angle);
+                                        
+                                    }
+                                }
+
+
+
+
+
+                                
+
+
+                                break;
+
+                            case GestureType.Drag:
+
+                                if (currentlySelectedObject)
+                                {
+                                    r = Camera.main.ScreenPointToRay(touch.position);
+
+                                    currentlySelectedObject.transform.position = r.origin + distanceToSelectedObject * r.direction;
+
+                                }
+                                break;
+
+                        }
+
+
+
+                        print("Moved");
+                        hasMoved = false;
+
+
+
+
+
+
+
+
+                        break;
+
+                    case TouchPhase.Ended:
+                        if ((timeTouched < 0.15f) && hasMoved)
+
+                        {
+                      
+                            if (Physics.Raycast(r, out hit))
+                            {
+                                CubeControl latestCube = hit.collider.gameObject.GetComponent<CubeControl>();
+
+                                if (latestCube)
+                                {
+                                    if (currentlySelectedObject != latestCube)
+                                    {
+                                        if (currentlySelectedObject) currentlySelectedObject.ToggleSelectThisCube();
+
+                                    }
+
+                                    currentlySelectedObject = latestCube;
+                                    latestCube.ToggleSelectThisCube();
+
+                                }
+
+
+
+
+                                print("hit");
+
+                            }
+
+                            else
+                            {
+
+                                print("here");
+                                if (currentlySelectedObject)
+                                {
+                                    currentlySelectedObject.ToggleSelectThisCube();
+                                    currentlySelectedObject = null;
+                                }
+                            }
+                        }
+                        if (currentlySelectedObject.gyroIsOn())
+                        {
+                            currentlySelectedObject.gyroOn(false);
+                        }
+                        else if (!currentlySelectedObject.gyroIsOn()) 
+                        {
+                            currentlySelectedObject.gyroOn(true);
+                        }
+                        print("Time Touch Held:" + timeTouched);
+
+                        
+
+                        break;
+
+                        
+
+                }
+                //currentlySelectedObject.gyroOn(true);
+
+
+                //switch (currentGesture)
+                //{
+                //    case GestureType.None:
+                //        break;
+                //    case GestureType.Tap:
+
+                //        if (isItaCube && (hit.collider.gameObject.GetComponent<CubeControl>() != currentlySelectedObject))
+                //        {
+                //            currentlySelectedObject = hit.collider.gameObject.GetComponent<CubeControl>();
+                //            currentlySelectedObject.ToggleSelectThisCube();
+                //            distanceToSelectedObject = Vector3.Distance(hit.collider.transform.position, transform.position);
+
+                //        }
+                //        else if (isItaCube && (hit.collider.gameObject.GetComponent<CubeControl>() == currentlySelectedObject))
+                //        {
+
+                //            currentlySelectedObject.ToggleSelectThisCube();
+                //            currentlySelectedObject = nullSelectedObject;
+                //        }
+
+
+                //        break;
+                //    case GestureType.Drag:
+
+
+
+                //        //Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+                //        //transform.Translate(touch.deltaPosition.y * speed, -touch.deltaPosition.x * speed, 0);
+
+
+                //        break;
+                //    case GestureType.Scale:
+
+                //        break;
+                //    case GestureType.Rotate:
+                //        break;
+                //    default:
+                //        break;
+                //}
+
+                //currentlySelectedObject.RotateCube(touch);
+
+
+
+
+                //pinch and zoom 2 touches
+                //if (Input.touchCount == 2)
+                //{
+                //    Touch touchZero = Input.GetTouch(0);
+                //    Touch touchOne = Input.GetTouch(1);
+
+                //    if (touchOne.phase == TouchPhase.Began)
+                //    {
+                //        startingDistanceBetweenTouches = Vector2.Distance(touchZero.position, touchOne.position);
+                //        startScale = currentlySelectedObject.Scale;
+
+                //    }
+                //    if ((touchOne.phase == TouchPhase.Moved) || (touchZero.phase == TouchPhase.Moved))
+                //    {
+                //        //Calculate distacne difference
+                //        float prevTouchDeltaMag = ((touchZero.position - touchZero.deltaPosition) - (touchOne.position - touchOne.deltaPosition)).magnitude;
+                //        float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+                //        float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+                //        print("magDiff: " + deltaMagnitudeDiff);
+
+                //        //Calculate Scale factor
+                //        float currentDistanceBetweenTouches = Vector2.Distance(touchZero.position, touchOne.position);
+                //        float scaleFactor = currentDistanceBetweenTouches / startingDistanceBetweenTouches;
+                //        print("scaleFactor: " + scaleFactor);
+
+                //        // Scale if selected
+                //        if (currentlySelectedObject)
+                //        {
+
+                //            currentlySelectedObject.Scale += -deltaMagnitudeDiff * 0.01f;
+                //        }
+                //        // Zoom if not selected
+                //        else
+                //        {
+
+                //            Camera.main.fieldOfView += deltaMagnitudeDiff * 0.1f;
+                //            Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, 20f, 100f);
+
+
+                //        }
+
+                //    }
+
+                //}
+
+
+
+            }
+
+
+
+            
+        }
+    }
+
+
+}
